e36dc0b40e6d90e0c0c59a85300dffeae07725c1
heynicecode <rickymuldrew@gmail.com>
2014-07-05 18:07:22 -0500

    Initialized the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DryrunApp::Application.config.secret_key_base = 'fb97daea0336fb01836db0031c9d1557bef5673f8d7a869d672aef373dce1008d112348a81286e8e50338da003ad8993c272d107d0f55a37c6d4d04f9fc29534'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DryrunApp::Application.config.session_store :cookie_store, key: '_dryrun_app_session'
